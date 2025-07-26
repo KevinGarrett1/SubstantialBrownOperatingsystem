c7e4dcf96fb9f3481b6ad59e651fe8e2c702625e
Kevin Garrett <mr.kevin.garrett@gmail.com>
2025-07-26 21:52:10 +0000

    Add timing logic to measure findNemo function performance with large arrays

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const namo1 = ['nemo'];
+const everyone = ['dolly', 'mitch' , 'sarah', 'susan'];
+const large = new Array(1000000).fill('nemo');
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") {
       console.log("Nemo Found!")
     }
   }
+  let t1 = performance.now();
+  console.log('Call to find Nemo took ' + (t1 - t0) + ' miliseconds');
 }
 
-findNemo(namo1);
+findNemo(large);
 
